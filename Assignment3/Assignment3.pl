4a248ccc4f133cfbfb9938a5c58d5fccd520d547
ryant26 <ryant26@gmail.com>
2016-03-07 22:40:55 -0700

    notmember seems to be working

--- Assignment3/Assignment3.pl
+++ Assignment3/Assignment3.pl
@@ -1,5 +1,22 @@
+% Question 1:
 xreverse([], []).
 
 xreverse([H|T], Y) :- 
 	xreverse(T, Reverse),
 	append(Reverse, [H], Y).
+
+% Question 2:
+xunique([], Lu).
+
+xunique([H|T], Lu) :-
+	notMember(H, Lu),
+	append(H, Lu, Lu),
+	xunique(T, Lu).
+ 
+
+notMember(E, []).
+
+notMember(E, [H|T]) :-
+	E \= H,
+	notMember(E, T).
+
