777a9f3c16e58d4dd2bdef0926bb6c140541113c
ryant26 <ryant26@gmail.com>
2016-03-12 21:03:58 -0700

    5.2 seems to be working. needs more testing

--- Assignment3/assignment3.pl
+++ Assignment3/assignment3.pl
@@ -1,4 +1,6 @@
-:- module(assignment3, [xreverse/2, xunique/2, xunion/3, removeLast/3, xsubset/2, clique/1, allConnected/1, allCliques/2, findCliques/2]).
+:- module(assignment3, [xreverse/2, xunique/2, xunion/3, removeLast/3, xsubset/2, 
+	clique/1, allConnected/1, allCliques/2, cliqueOfLength/2, cliqueGreaterThan/2, difference/3,
+	notSubset/3, maxClique/2]).
 
 :- use_module(graphs).
 
@@ -50,17 +52,31 @@ xsubset([X|Xs], Set) :-
   xsubset(Xs, Set1).
 
 % Question 5.2:
-% maxClique()
+ maxClique(X, Cliques) :-
+ 	findall(H, cliqueOfLength(X, H), LengthCliques),
+ 	findall(H, cliqueGreaterThan(X, H), GreaterCliques),
+ 	notSubset(GreaterCliques, LengthCliques, Cliques).
 
-allCliques(L, Out) :-
-	findall(Subset, xsubset(Subset, L), Subsets),
-	findCliques(Subsets, Out).
 
-findCliques([], _).
+cliqueOfLength(X,L) :-
+	clique(L),
+	length(L,Length),
+	Length == X.
 
-findCliques([H1|T1], [H1|T2]) :-
-	clique(H1),
-	findCliques(T1, T2).
+cliqueGreaterThan(X, L) :-
+	clique(L),
+	length(L, Length),
+	Length > X.
 
-findCliques([_|T1], L) :-
-	findCliques(T1, L).
+notSubset([], L2, L2).
+ 
+notSubset([H1|T], L2, Out) :-
+	findall(X, xsubset(X, H1), Subsets),
+	difference(Subsets, L2, Lp),
+	notSubset(T, Lp, Out).
+
+difference([], L2, L2).
+
+difference([H|T], L2, L3) :-
+	delete(L2, H, Lp),
+	difference(T, Lp, L3).
