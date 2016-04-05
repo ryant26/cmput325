823f1083757ec933604ec7c0b9c6c0b0b9543856
ryan <ryant26@gmail.com>
2016-04-05 17:14:41 -0600

    Working soltion to Q2

--- Assignment4/a4.pl
+++ Assignment4/a4.pl
@@ -3,24 +3,26 @@
 disarm([], [], []).
 disarm(A, B, [H3|R3]) :-
 	findElement(A, B, L, E),
-	H3 = [E, L],
+	H3 = [[E], L],
 	removeAll(L, A, Aresult),
 	select(E, B, Bresult),
-	disarm(Aresult, Bresult, R3).
+	disarm(Aresult, Bresult, R3), 
+	sortedCheck([H3|R3]),!.
 
 disarm(A, B, [H3|R3]) :-
 	findElement(B, A, L, E),
-	H3 = [E, L],
+	H3 = [[E], L],
 	removeAll(L, B, Bresult),
 	select(E, A, Aresult),
-	disarm(Aresult, Bresult, R3).
+	disarm(Aresult, Bresult, R3),
+	sortedCheck([H3|R3]),!.
 
 
 findElement(A, B, L, E):-
-	member(X, A),
-	member(Y, A),
-	X \== Y,
+	select(X, A, ResA),
+	member(Y, ResA),
 	member(E, B),
+	X =< Y,
 	E is X + Y,
 	L = [X, Y].
 
@@ -28,3 +30,9 @@ removeAll([], R, R).
 removeAll([H|R], List, Result) :-
 	select(H, List, Res),
 	removeAll(R, Res, Result).
+
+sortedCheck([]).
+sortedCheck([_]).
+sortedCheck([[[H1], _], [[H2], _]|R]) :-
+	H1 =< H2,
+	sortedCheck(R).
