35a82014a8bbd8b7dd13bc368e66ba025cbd020c
ryan <ryant26@gmail.com>
2016-04-05 15:49:10 -0600

    partial solution to 2

--- Assignment4/a4.pl
+++ Assignment4/a4.pl
@@ -1,2 +1,30 @@
 % Question 2
 % ==========
+disarm([], [], []).
+disarm(A, B, [H3|R3]) :-
+	findElement(A, B, L, E),
+	H3 = [E, L],
+	removeAll(L, A, Aresult),
+	select(E, B, Bresult),
+	disarm(Aresult, Bresult, R3).
+
+disarm(A, B, [H3|R3]) :-
+	findElement(B, A, L, E),
+	H3 = [E, L],
+	removeAll(L, B, Bresult),
+	select(E, A, Aresult),
+	disarm(Aresult, Bresult, R3).
+
+
+findElement(A, B, L, E):-
+	member(X, A),
+	member(Y, A),
+	X \== Y,
+	member(E, B),
+	E is X + Y,
+	L = [X, Y].
+
+removeAll([], R, R). 
+removeAll([H|R], List, Result) :-
+	select(H, List, Res),
+	removeAll(R, Res, Result).
